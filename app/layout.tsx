1420bd2d144d64ef2959fd2d16285edcf09969cf
Het Gajera <het36100@gmail.com>
2024-09-07 00:57:34 +0530

    Update home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,28 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import { ClerkProvider} from '@clerk/nextjs'
-import { neobrutalism } from '@clerk/themes'
-import './globals.css'
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
 
 const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "File.ai",
   description: "Chat with pdf",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <ClerkProvider 
-    appearance={{
-      baseTheme: [neobrutalism]
-    }}>
+    <ClerkProvider>
       <html lang="en">
-        <body>
-          {children}
-        </body>
+        <body>{children}</body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
